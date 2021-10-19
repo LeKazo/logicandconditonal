61c64b5b52dc5351704cfcda827298c371675c5a
LeKazo <70152390+LeKazo@users.noreply.github.com>
2021-10-19 10:03:11 -0400

    Create program1.cs

---
+++ program1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a movie genre: ");
+            string genre = Console.ReadLine();
+
+            switch (genre)
+            {
+                case "Drama":
+                    Console.WriteLine("Citizen Kane");
+                    break;
+
+                case "Comedy":
+                    Console.WriteLine("Duck Soup");
+                    break;
+
+                case "Adventure":
+                    Console.WriteLine("King Kong");
+                    break;
+
+                case "Horror":
+                    Console.WriteLine("Psycho");
+                    break;
+
+                case "Science Fiction":
+                    Console.WriteLine("2001: A Space Odyssey");
+                    break;
+
+                default:
+                    Console.WriteLine("No movie found");
+                    break;
+            }
+
+        }
+    }
+}
